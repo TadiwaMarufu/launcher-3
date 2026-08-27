e1d489db86e507254773092d859b8277af50229d
TadiwaMarufu <tadiwamarufu00@gmail.com>
2026-08-27 17:37:49 +0200

    Refactor MainActivity to use MaterialTheme
    Updated the UI theme to use MaterialTheme and added Surface for background.

--- app/src/main/java/com/emo/launcher/MainActivity.kt
+++ app/src/main/java/com/emo/launcher/MainActivity.kt
@@ -5,10 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import com.emo.launcher.ui.EmoLauncherApp
-import com.emo.launcher.ui.LauncherTheme
 
 class MainActivity : ComponentActivity() {
 
@@ -20,14 +21,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            LauncherTheme {
 
-                val apps by viewModel.apps.collectAsState()
+            val apps by viewModel.apps.collectAsState()
 
-                EmoLauncherApp(
-                    apps = apps,
-                    onLaunchApp = viewModel::launch
-                )
+            MaterialTheme {
+
+                Surface(
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    EmoLauncherApp(
+                        apps = apps,
+                        onLaunchApp = viewModel::launch
+                    )
+                }
             }
         }
     }
