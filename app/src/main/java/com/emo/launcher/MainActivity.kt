2eeb2508196648b6dda8b736a1ef47fa380fcd44
TadiwaMarufu <tadiwamarufu00@gmail.com>
2026-08-27 17:21:07 +0200

    Integrate ViewModel and update EmoLauncherApp usage

--- app/src/main/java/com/emo/launcher/MainActivity.kt
+++ app/src/main/java/com/emo/launcher/MainActivity.kt
@@ -4,14 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import com.emo.launcher.ui.EmoLauncherApp
+import com.emo.launcher.ui.LauncherTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: LauncherViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContent {
-            EmoLauncherApp()
+            LauncherTheme {
+
+                val apps by viewModel.apps.collectAsState()
+
+                EmoLauncherApp(
+                    apps = apps,
+                    onLaunchApp = viewModel::launch
+                )
+            }
         }
     }
 }
